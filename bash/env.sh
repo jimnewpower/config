ef2e7dae5a04ca8549f3482e1ce12cd2ade8e0f6
Jim Newpower <jimnewpower@gmail.com>
2023-01-26 09:44:13 -0700

    PRA dev environment variables

---
+++ bash/env.sh
@@ -0,0 +1,9 @@
+# PRA develop environment
+export SRC_TREE="/home/jim/develop/basinmod"
+export DEBUG="/home/jim/develop/basinmod/debug.txt"
+export CONFIG_HOST_FILENAME=jim-Predator-PH317-54.mk
+export PLATTE_RIVER_DEBUG=xyzzy
+# export RELEASE=t
+export NOPCINT=t
+export TEMP="/home/jim/tmp"
+
